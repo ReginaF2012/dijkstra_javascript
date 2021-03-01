864684f2ef6260170177e78650f71163886fb81d
ReginaF2012 <skritschy@gmail.com>
2021-02-28 21:38:32 -0600

    removed priority queue, just used vertexWithMinDistance method

--- dijkstra.js
+++ dijkstra.js
@@ -6,134 +6,75 @@ class Graph {
 
     addVertex(vertex) {
         this.vertices.push(vertex);
-        this.adjacencyList[vertex] = []
+        this.adjacencyList[vertex] = {};
     }
 
     addEdge(vertex1, vertex2, weight) {
-        this.adjacencyList[vertex1].push({vertex: vertex2, weight: weight});
-        this.adjacencyList[vertex2].push({vertex: vertex1, weight: weight});
+        this.adjacencyList[vertex1][vertex2] = weight;
+    }
+
+    changeWeight(vertex1, vertex2, weight) {
+        this.adjacencyList[vertex1][vertex2] = weight;
     }
 
     dijkstra(source) {
-        let distances = [], queue = new PriorityQueue();
+        let distances = {},
+            paths = {},
+            visited = {};
+        for (let i = 0; i < this.vertices.length; i++) {
+            if (this.vertices[i] === source) {
+                distances[source] = 0;
+                paths[this.vertices[i]] = 'source';
+            } else {
+                distances[this.vertices[i]] = Infinity;
+                paths[this.vertices[i]] = source;
+            }
+            visited[this.vertices[i]] = false;
+        }
+        
+        let currVertex = this.vertexWithMinDistance(distances, visited);
 
+        while (currVertex !== null) {
+            let distance = distances[currVertex],
+                neighbors = this.adjacencyList[currVertex];
+            for (let neighbor in neighbors) {
+                let newDistance = distance + neighbors[neighbor];
+                if (distances[neighbor] > newDistance) {
+                    distances[neighbor] = newDistance;
+                    paths[neighbor] = currVertex;
+                }
+            }
+            visited[currVertex] = true;
+            currVertex = this.vertexWithMinDistance(distances, visited);
+        }
 
+        console.log(distances);
+        console.log(paths);
+    }
+
+    vertexWithMinDistance(distances, visited) {
+        let minDistance = Infinity,
+            minVertex = null;
+        for (let vertex in distances) {
+            let distance = distances[vertex];
+            if (distance < minDistance && !visited[vertex]) {
+                minDistance = distance;
+                minVertex = vertex;
+            }
+        }
+        return minVertex;
     }
 }
 
-// class PriorityQueue {
-//     constructor(array = []) {
-//         this.values = [];
-//         if (array.length > 0) this.buildHeap(array);
-//     }
+let g = new Graph();
+g.addVertex("start");
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("finish");
 
-//     // index of the parent node
-//     parent(index) {
-//         return Math.floor((index - 1) / 2);
-//     }
-
-//     // index of the left child node
-//     leftChild(index) {
-//         return (index * 2) + 1;
-//     }
-
-//     // index of the right child node
-//     rightChild(index) {
-//         return (index * 2) + 2;
-//     }
-
-//     // returns true if index is of a node that has no children
-//     isLeaf(index) {
-//         return (
-//             index >= Math.floor(this.values.length / 2) && index <= this.values.length - 1
-//         )
-//     }
-
-//     // swap using ES6 destructuring
-//     swap(index1, index2) {
-//         [this.values[index1], this.values[index2]] = [this.values[index2], this.values[index1]];
-//     }
-
-
-//     heapifyDown(index) {
-
-//         // if the node at index has children
-//         if (!this.isLeaf(index)) {
-
-//             // get indices of children
-//             let leftChildIndex = this.leftChild(index),
-//                 rightChildIndex = this.rightChild(index),
-
-//                 // start out smallest index at parent index
-//                 smallestIndex = index;
-
-//             // if the left child < parent
-//             if (this.values[leftChildIndex]?.priority < this.values[smallestIndex]?.priority) {
-//                 // reassign smallest index to left child index
-//                 smallestIndex = leftChildIndex;
-//             }
-
-//             // if the right child < element at smallest index (either parent or left child)
-//             if (this.values[rightChildIndex]?.priority <= this.values[smallestIndex]?.priority) {
-//                 // reassign smallest index to right child index
-//                 smallestIndex = rightChildIndex;
-//             }
-
-//             // if the smallest index is not the parent index
-//             if (smallestIndex !== index) {
-//                 // swap
-//                 this.swap(index, smallestIndex);
-//                 // recursively move down the heap
-//                 this.heapifyDown(smallestIndex);
-//             }
-//         }
-//     }
-
-//     heapifyUp(index) {
-//         let currentIndex = index,
-//             parentIndex = this.parent(currentIndex);
-
-//         // while we haven't reached the root node and the current element is greater than its parent node
-//         while (currentIndex > 0 && this.values[currentIndex]?.priority < this.values[parentIndex]?.priority) {
-//             // swap
-//             this.swap(currentIndex, parentIndex);
-//             // move up the binary heap
-//             currentIndex = parentIndex;
-//             parentIndex = this.parent(parentIndex);
-//         }
-//     }
-
-//     enqueu(value, priority) {
-//         // add element to the end of the heap
-//         this.values.push({value, priority});
-//         // move element up until it's in the correct position
-//         this.heapifyUp(this.values.length - 1);
-//     }
-
-//     // removes and returns max element
-//     dequeue() {
-//         if (this.values.length < 1) return 'empty queue';
-
-//         // get min
-//         const min = this.values[0];
-//         const end = this.values.pop();
-
-//         if (this.values.length > 0) {
-//             // reassign first element to the last element
-//             this.values[0] = end;
-//             // heapify down until element is back in its correct position
-//             this.heapifyDown(0);
-//         }
-
-//         // return the max
-//         return min;
-//     }
-
-//     buildHeap(array) {
-//         this.values = array;
-//         // since leaves start at floor(nodes / 2) index, we work from the leaves up the heap
-//         for(let i = Math.floor(this.values.length / 2); i >= 0; i--){
-//             this.heapifyDown(i);
-//         }
-//     }
-// }
+g.addEdge("start", "A", 6);
+g.addEdge("start", "B", 2);
+g.addEdge("A", "finish", 1);
+g.addEdge("B", "A", 3);
+g.addEdge("B", "finish", 5)
+g.dijkstra("start");
